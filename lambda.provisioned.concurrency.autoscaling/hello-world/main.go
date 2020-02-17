a11e2f9910736de63f76f7259e6f652798dae94f
Waldemar Bira <waldek.bira@gmail.com>
2020-02-17 17:17:53 +0100

    Created lambda provisioned concurrency autoscaling

---
+++ lambda.provisioned.concurrency.autoscaling/hello-world/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprintf("Hello provisioned concurrency"),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
